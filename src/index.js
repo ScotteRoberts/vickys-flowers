1771ef408022dd4c19f8a8ff7c86523d86cf7b59
Scott Roberts <s.e.roberts0@gmail.com>
2020-01-15 15:57:07 -0800

    Getting closer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import 'react-app-polyfill/ie11';
+import '@babel/polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './main.css';
 import App from './components/app';
-import '@babel/polyfill';
 
 ReactDOM.render(<App />, document.getElementById('root'));
