503f8fb3f214238a103ac25d15b468f98a796526
Scott Roberts <s.e.roberts0@gmail.com>
2020-01-15 15:44:57 -0800

    @Babel/polyfill

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './main.css';
 import App from './components/app';
-import 'babel-polyfill';
+import '@babel/polyfill';
 
 ReactDOM.render(<App />, document.getElementById('root'));
