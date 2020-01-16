fe0d71641debe874815d2f14e6c6ce1ebd374fa4
Scott Roberts <s.e.roberts0@gmail.com>
2020-01-16 10:05:53 -0800

    Cart adjustments and color changes

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -3,9 +3,37 @@ import PropTypes from 'prop-types';
 import { useDrop } from 'react-dnd';
 import './Cart.css';
 
+/**
+ * Dynamically returns an object of style properties based on the number of cart items.
+ * @warning Relies on the icon being the same size
+ * @param {Number} numItems The number of cart items
+ * @returns Style object
+ */
+const cartItemSize = numItems => {
+  if (numItems < 10) {
+    return {
+      fontSize: '16px',
+      left: '15px',
+      top: '4px',
+    };
+  } else if (numItems < 100) {
+    return {
+      fontSize: '14px',
+      left: '12px',
+      top: '5px',
+    };
+  } else {
+    return {
+      fontSize: '12px',
+      left: '10px',
+      top: '6px',
+    };
+  }
+};
+
 const Cart = props => {
   // FIXME: Figure out how to set this to decimal formatting
-  const [totalAmount, setTotalAmount] = useState(0);
+  const [totalAmount, setTotalAmount] = useState(35);
   const [totalItems, setTotalItems] = useState(0);
   const [{ canDrop }, drop] = useDrop({
     accept: 'flower',
@@ -19,9 +47,14 @@ const Cart = props => {
 
   return (
     <div className="cart">
-      TOTAL AMOUNT: $<span>{totalAmount.toFixed(2)}</span> |{' '}
+      <div>
+        TOTAL AMOUNT: $<span>{totalAmount.toFixed(2)}</span>
+        <span className="tabbed-vertical-separator">|</span>
+      </div>
       <div ref={drop} className="cart-icon-container">
-        <span className="total-items">{totalItems}</span>
+        <span className="total-items" style={cartItemSize(totalItems)}>
+          {totalItems}
+        </span>
         <img
           src={require('../../assets/img/page-1-2.svg')}
           alt="Cart icon"
--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -7,8 +7,8 @@ import ShopItem from '../shop-item';
 
 const Shop = props => (
   <main className="shop">
-    <h1>Top Products</h1>
-    <p>
+    <h1 className="title">Top Products</h1>
+    <p className="subtitle">
       To add desired products to the cart, simply drag a product image to the
       cart icon found at the top right of the page.
     </p>
