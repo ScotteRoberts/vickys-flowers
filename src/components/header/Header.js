245932c0b85d81c472fc9fdcd95c1551dbca61d9
Scott Roberts <s.e.roberts0@gmail.com>
2020-01-16 16:25:38 -0800

    Compact Navigation

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,60 +1,20 @@
 import React, { useState } from 'react';
-import { Link } from '@reach/router';
 import PropTypes from 'prop-types';
+import { useMediaQuery } from 'react-responsive';
 import './Header.css';
 
-import Cart from '../cart';
+import { Cart } from '../cart';
+import { NavBar, CompactNavBar } from '../navbar';
 
-const CompactNav = () => {
-  const [active, setActive] = useState(false);
+const Header = props => {
+  const isCompact = useMediaQuery({ query: '(max-width: 1000px)' });
   return (
-    <nav className="full-nav">
-      <div className="responsive-nav-items-container">
-        <button className="nav-icon-button" onClick={() => setActive(!active)}>
-          &#8801;
-        </button>
-        <nav className={`responsive-nav-items${active ? ' active' : ''}`}>
-          <Link to="/">HOME</Link>
-          <Link to="/about">ABOUT US</Link>
-          <Link to="/occasions">OCCASSIONS</Link>
-          <Link to="/order">ORDER</Link>
-        </nav>
-      </div>
-
-      <Link className="store-icon-container" to="/">
-        <img
-          className="icon"
-          src={require('../../assets/img/page-1.svg')}
-          alt="Vicky's Flowers icon"
-        />
-      </Link>
-    </nav>
+    <header className="header">
+      {isCompact ? <CompactNavBar /> : <NavBar />}
+      <Cart />
+    </header>
   );
 };
-
-const FullNav = () => (
-  <nav className="full-nav">
-    {/* TODO: Get back to making this responsive */}
-    <Link className="store-icon-container" to="/">
-      <img
-        className="icon"
-        src={require('../../assets/img/page-1.svg')}
-        alt="Vicky's Flowers icon"
-      />
-    </Link>
-    <Link to="/">HOME</Link>
-    <Link to="/about">ABOUT US</Link>
-    <Link to="/occasions">OCCASSIONS</Link>
-    <Link to="/order">ORDER</Link>
-  </nav>
-);
-
-const Header = props => (
-  <header className="header">
-    <FullNav />
-    <Cart />
-  </header>
-);
 
 Header.propTypes = {};
 
